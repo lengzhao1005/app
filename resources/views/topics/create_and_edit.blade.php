32c23e9a01891d1b73c8f137e5403bbc6b5a78ea
lzcompany <lzcompany@qq.com>
2017-12-26 18:43:45 +0800

    simditor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -58,29 +58,21 @@
 @endsection
 
 @section('styles')
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('editor/simditor.css') }}">
 @stop
 
 @section('scripts')
-    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('editor/simditor.js') }}"></script>
 
-{{--    <script>
+    <script>
     $(document).ready(function(){
         var editor = new Simditor({
             textarea: $('#editor'),
-            upload: {
-                url: '/',
-                params: { _token: '{{ csrf_token() }}' },
-                fileKey: 'upload_file',
-                connectionCount: 3,
-                leaveConfirm: '文件上传中，关闭此页面将取消上传。'
-            },
-            pasteImage: true,
         });
     });
-    </script>--}}
+    </script>
 
 @stop
