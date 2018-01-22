89d0350cb0db862cb9230fb3c974816ae27b4b54
lzcompany <lzcompany@qq.com>
2018-01-22 09:13:02 +0800

    old

--- app/Http/Controllers/HlalaController.php
+++ app/Http/Controllers/HlalaController.php
@@ -28,8 +28,8 @@ class HlalaController extends Controller
 
         $post_data=['form_params'=>['grant_type'=>'client_credentials','scope'=>'read']];
 
-        //$url = 'http://auth.hualala.com/oauth/token?grant_type=client_credentials&scope=read';
-        $url = 'http://auth.hualala.com/oauth/token';
+        $url = 'http://auth.hualala.com/oauth/token?grant_type=client_credentials&scope=read';
+        //$url = 'http://dohko.auth.hualala.com/oauth/token';
 
         $respon = $http->request('POST',$url,$post_data);
 
@@ -41,45 +41,45 @@ class HlalaController extends Controller
         cache('hualala_token',$res['access_token']);
         return $res['access_token'];
 
-        /*//dGlhbm1pbWk6aFFXMUVGRUZZMjNTSGI=
+        //dGlhbm1pbWk6aFFXMUVGRUZZMjNTSGI=
         //dd(base64_encode('guozl:NhzDcu46ert2Nbv'));
-        $request = [
-            'grant_type'=>'client_credentials',
-            'scope'=>'read'
-        ];
-
-        $post_data = http_build_query($request);
-        $url = "http://auth.hualala.com/oauth/token";
-
-        $headers = ['Authorization: Basic Z3Vvemw6Tmh6RGN1NDZlcnQyTmJ2='];
-
-        $request = [
-            'grant_type'=>'client_credentials',
-            'scope'=>'read'
-        ];
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-
-        // post数据
-        curl_setopt($ch, CURLOPT_POST, 1);
-        // post的变量
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-
-        $output = curl_exec($ch);
-        curl_close($ch);
-
-        dd($output);*/
+//        $request = [
+//            'grant_type'=>'client_credentials',
+//            'scope'=>'read'
+//        ];
+//
+//        $post_data = http_build_query($request);
+//        $url = "http://auth.hualala.com/oauth/token";
+//
+//        $headers = ['Authorization: Basic Z3Vvemw6Tmh6RGN1NDZlcnQyTmJ2='];
+//
+//        $request = [
+//            'grant_type'=>'client_credentials',
+//            'scope'=>'read'
+//        ];
+//
+//        $ch = curl_init();
+//
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+//
+//         post数据
+//        curl_setopt($ch, CURLOPT_POST, 1);
+//         post的变量
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+//
+//        $output = curl_exec($ch);
+//        curl_close($ch);
+//
+//        dd($output);
     }
 
     public function getOrderList($token,$secret)
     {
-        $url = 'http://open-api.hualala.com/getOrderFoodAndPayInfo';
+        $url = 'http://open-api.hualala.com/card/queryCardInfo';
 
         $http = new Client([
             'headers' => [
@@ -90,21 +90,25 @@ class HlalaController extends Controller
         $time = time();
 
         $should_data = [
-            'groupID'=>'31457',
-            'shopID'=>'76075780',
-            'timestamp'=>$time,
-            'reportDate'=>date('Y-m-d H:i:s',$time)
+            'timestamp' => $time,//时间戳,用户签名校验
+            'groupID' =>31457,//集团 ID
+            'shopID' => 76075780,//店铺 ID
+            'sourceWay' => 1,//请求来源，1
+            'sourceType' => 30,//会员来源类型 81
+            'cardTypeID' => '800330942903242752',//卡类型 ID
         ];
 
         $sign = $this->getSign($should_data,$secret);
 
         $post_data=[
             'form_params'=>[
-                'groupID'=>'31457',
-                'shopID'=>'76075780',
-                'timestamp'=>$time,
+                'timestamp' => $time,//时间戳,用户签名校验
+                'groupID' =>31457,//集团 ID
+                'shopID' => 76075780,//店铺 ID
+                'sourceWay' => 1,//请求来源，1
+                'sourceType' => 30,//会员来源类型 81
+                'cardTypeID' => 800330942903242752,//卡类型 ID
                 'signature'=>$sign,
-                'reportDate'=>date('Y-m-d H:i:s',$time)
             ]
         ];
 
@@ -132,3 +136,4 @@ class HlalaController extends Controller
 
     }
 }
+
