4dc5f51dc60e177aa2c5d12fc79827e0f738ccec
lz <lz@qq.com>
2018-05-15 21:15:03 +0800

    tate limit

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,12 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',[
     'namespace'=>'App\Http\Controllers\Api',
 ],function($api){
+
+    $api->group([
+        'middleware'=>'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ],function($api){
         //短信验证码
         $api->post('verificationCodes','VerificationCodesController@store')
             ->name('api.verificationCodes.store');
@@ -30,7 +36,7 @@ $api->version('v1',[
             ->name('api.users.store');
 
 
-
+    });
 
 });
 
